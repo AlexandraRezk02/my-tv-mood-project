ae7d05a9dd0365e55a2c7435f8647b0843c53b70
Alexandra Rezk <alexandrarezk@gmail.com>
2017-08-11 12:37:58 -0400

    Updated code in the random-button.js file

--- js/random-button.js
+++ js/random-button.js
@@ -1,28 +1,13 @@
 (function(){
-	var $imgs = $('#dvdGallery img'); 							//Stores all images
-	var $buttons = $('#buttons');								//Stores button elements
-	var myMovies = [];											//Create empty DVD array
+	var $imgs = $('#dvdGallery img'); 					//Stores all images
+	var $buttons = $('#buttons');						//Stores button elements
+	var myMovies = [];									//Create empty DVD array
     var previousDVD;
 	
-	$imgs.each(function(){										//Loop through images
-	var img = this;											//Store image in variable
-		myMovies.push(img);									//Stores images in array
+	$imgs.each(function(){								//Loop through images
+	var img = this;										//Store image in variable
+	myMovies.push(img);									//Stores images in array
 	});
-	
-	Array.prototype.shuffle = function() {
-		var input = this;
-		for (var i = input.length-1; i >=0; i--){
-			var randomIndex = Math.floor(Math.random() * (i+1));
-			var itemAtIndex = input[randomIndex];
-			
-			input[randomIndex] = input[i];
-			input[i] = itemAtIndex;
-		}
-		
-		return input;
-	}
-	
-	
 	
 	//Buttons, event handlers, and filters go here
 	$('<button/>', {
@@ -33,7 +18,9 @@
 			.addClass('active')
 			.siblings()
 			.removeClass('active');
-			var randomDVD = myMovies[Math.floor(Math.random() * myMovies.length)]; //Generate a random number based on the myMovies array
+			//Generate a random number based on the myMovies array
+			var randomDVD = 
+				myMovies[Math.floor(Math.random() * myMovies.length)]; 
 			$imgs
 			.hide()
 			.filter(randomDVD)
